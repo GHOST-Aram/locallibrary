2706c71f4ba55b4342d885936d022530a7b682f9
GHOST-Aram <owinoojala@gmail.com>
2023-06-23 15:20:11 +0300

    chore: Import unresolved function       -Import reverse from django.urls

--- catalogue/models/author_models.py
+++ catalogue/models/author_models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib import admin
+from django.urls import reverse
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
@@ -8,19 +10,19 @@ class Author(models.Model):
     date_of_death = models.DateField(null=True, blank=True)
 
     class Meta:
-        ordering = ['first_name', 'last_name']
+        ordering = ["first_name", "last_name"]
 
     def get_absolute_url(self):
-        return reverse('author-detail', args=[str(id)])
-    
-    
+        return reverse("author-detail", args=[str(id)])
+
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return f"{self.first_name} {self.last_name}"
+
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'date_of_birth', 'date_of_death_view')
-    fields = [('first_name', 'last_name'), ('date_of_birth', 'date_of_death')]
+    list_display = ("__str__", "date_of_birth", "date_of_death_view")
+    fields = [("first_name", "last_name"), ("date_of_birth", "date_of_death")]
 
-    @admin.display(empty_value='Date unknown')
+    @admin.display(empty_value="Date unknown")
     def date_of_death_view(self, author):
         return author.date_of_death
--- catalogue/models/book_models.py
+++ catalogue/models/book_models.py
@@ -1,37 +1,39 @@
-from django.db import models
 from django.contrib import admin
+from django.db import models
 from .genre_models import Genre
+from django.urls import reverse
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
     summary = models.TextField(
-        max_length=1000, help_text='Enter a brief desciption of the book'
+        max_length=1000, help_text="Enter a brief desciption of the book"
     )
     isbn = models.CharField(
-        'ISBN', max_length=13, unique=True, 
+        "ISBN",
+        max_length=13,
+        unique=True,
         help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">'
-        +'ISBN number</a>'
-        )
-    genre = models.ManyToManyField(
-        Genre, help_text='Select a genre for this book.'
-        )
-    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
-    
+        + "ISBN number</a>",
+    )
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book.")
+    language = models.ForeignKey("Language", on_delete=models.SET_NULL, null=True)
+
     def __str__(self):
-        return f'{self.title}'
-    
+        return f"{self.title}"
+
     def get_absolute_url(self):
-        return reverse('book-detail', args=[str(self.id)])
-    
+        return reverse("book-detail", args=[str(self.id)])
+
     def display_genre(self):
-        return ', '.join(genre.name for genre in self.genre.all()[:2])
-    
-    display_genre.short_description = 'Genre'
-    
+        return ", ".join(genre.name for genre in self.genre.all()[:2])
+
+    display_genre.short_description = "Genre"
+
+
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('isbn', '__str__','author', 'display_genre')
-    empty_value_display = 'Unknown'
-    list_filter = ['author', 'language']
-    fields = [('title', 'author', 'isbn'), 'summary', ('genre', 'language')]
-    
+    list_display = ("isbn", "__str__", "author", "display_genre")
+    empty_value_display = "Unknown"
+    list_filter = ["author", "language"]
+    fields = [("title", "author", "isbn"), "summary", ("genre", "language")]
